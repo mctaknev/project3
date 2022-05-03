c0ac9cf2a6c557603a4e39cd44560cb9fc98e75c
mctaknev <100849698+mctaknev@users.noreply.github.com>
2022-05-03 09:33:34 +0530

    first commit

---
+++ hai.js
@@ -0,0 +1,7 @@
+let container = document.getElementById("Container");
+
+
+let javas = document.getElementById("javas");
+javas.textContent="HELLO JAVASCRIPT";
+javas.classList.add("javas");
+container.appendChild(javas);
